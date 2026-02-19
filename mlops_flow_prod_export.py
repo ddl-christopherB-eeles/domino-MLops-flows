e3003647b7b93c2145b891451f9cbd1ff5f8fc70
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2026-02-18 17:16:25 -0800

    Pushed from Domino: https://rliucp108882.engineering-dev.domino.tech/u/integration-test/mlops-flows/workspaces?executionId=69966431349a4e059bd58276

--- mlops_flow_prod_export.py
+++ mlops_flow_prod_export.py
@@ -5,18 +5,19 @@ from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT, ExportArtifactToDatasetsSpec, run_launch_export_artifacts_task
 
+# pyflyte run --remote mlops_flow_prod_export.py model_training --data_path_a /mnt/code/data/datasetA.csv --data_path_b /mnt/code/data/datasetB.csv
 
 # As this is considered a PROD Flow definition, we do not the use_project_defaults_for_omitted parameter
 # and explictly set every required parameter in the task defintion to ensure reproducability.
 # These are the additional parameters that need to be explicitly set of each task. 
 
-environment_name="Domino Standard Environment Py3.10 R4.5 - Latest Cloud Release"  # Change to the name of your deployments Domino Standard Environment
-environment_revision_id="68cb02b5536add3e634e7cd1"              # Change to the latest revision ID of your deployments Domino Standard Environment
+environment_name="Domino Standard Environment Py3.10 R4.5"  # Change to the name of your deployments Domino Standard Environment
+environment_revision_id="694445374260132a9f000b4a"              # Change to the latest revision ID of your deployments Domino Standard Environment
 hardware_tier_name="Small"                                 # Change to the name of one of your Domino's hardware tiers
 GitRef_type="commitId"                                     
-GitRef_value="c4f273e63e3267d079ebb7c5e5dbcef600521829"   # Change to the commitId of main Git repository 
+GitRef_value="ae248799f3306c05b42dd4f685a87132556a953a"   # Change to the commitId of main Git repository 
 volume_size_gib=10
-dfs_repo_commit_id="8add2e00c1046b552149b669fd728de488b4d001"   # Change to the latest commit ID of the Artifacts file system in your project
+dfs_repo_commit_id="5995b7bcbba406b88bc67ab419b02e61093fa31f"   # Change to the latest commit ID of the Artifacts file system in your project
 
 
 # Set if you want caching on or off for all your tasks.
@@ -26,6 +27,7 @@ cache=False
 DataArtifact = Artifact("Merged Data", DATA)
 ModelArtifact = Artifact("Random Forest Model", MODEL)
 
+# rliu
 @workflow
 def model_training(data_path_a: str, data_path_b: str): 
     '''
@@ -44,13 +46,14 @@ def model_training(data_path_a: str, data_path_b: str):
 
     task1 = run_domino_job_task(
         flyte_task_name='Load Data A',
-        command='python /mnt/code/scripts/load-data-A.py',
+        command='ls -lart /workflow && ls -lart /workflow/data && /mnt/code/scripts/setup.sh && ls -lart /workflow && ls -lart /workflow/data && python /mnt/code/scripts/load-data-A.py',
         inputs=[Input(name='data_path', type=str, value=data_path_a)],
         output_specs=[Output(name='datasetA', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
         environment_revision_id=environment_revision_id,
         hardware_tier_name=hardware_tier_name,
-        dataset_snapshots=[],
+        dataset_snapshots=[DatasetSnapshot(Id="6983829c26a4823186cd8b40", Version=1, Name="mlops-flows")],
+        netapp_volume_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
@@ -61,13 +64,14 @@ def model_training(data_path_a: str, data_path_b: str):
 
     task2 = run_domino_job_task(
         flyte_task_name='Load Data B',
-        command='python /mnt/code/scripts/load-data-B.py',
+        command='ls -lart /workflow && ls -lart /workflow/data && /mnt/code/scripts/setup.sh && ls -lart /workflow && ls -lart /workflow/data && python /mnt/code/scripts/load-data-B.py',
         inputs=[Input(name='data_path', type=str, value=data_path_b)],
         output_specs=[Output(name='datasetB', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
         environment_revision_id=environment_revision_id,
         hardware_tier_name=hardware_tier_name,
-        dataset_snapshots=[],
+        dataset_snapshots=[DatasetSnapshot(Id="6983829c26a4823186cd8b40", Version=1, Name="mlops-flows")],
+        netapp_volume_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
@@ -78,15 +82,16 @@ def model_training(data_path_a: str, data_path_b: str):
 
     task3 = run_domino_job_task(
         flyte_task_name='Merge Data',
-        command='python /mnt/code/scripts/merge-data.py',
+        command='ls -lart /workflow && ls -lart /workflow/data && /mnt/code/scripts/setup.sh && ls -lart /workflow && ls -lart /workflow/data && python /mnt/code/scripts/merge-data.py',
         inputs=[
             Input(name='datasetA', type=FlyteFile[TypeVar('csv')], value=task1['datasetA']),
             Input(name='datasetB', type=FlyteFile[TypeVar('csv')], value=task2['datasetB'])],
-        output_specs=[Output(name='merged_data', type=DataArtifact.File(name="merged_data.csv"))],
+        output_specs=[Output(name='merged_data', type=DataArtifact.File(name="merged_data.thisisareallylongfiletype"))],
         environment_name=environment_name,
         environment_revision_id=environment_revision_id,
         hardware_tier_name=hardware_tier_name,
         dataset_snapshots=[],
+        netapp_volume_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
@@ -95,15 +100,18 @@ def model_training(data_path_a: str, data_path_b: str):
         cache_version="1.0"
     )
 
+    # import pdb; pdb.set_trace()
+
     task4 = run_domino_job_task(
         flyte_task_name='Process Data',
-        command='python /mnt/code/scripts/process-data.py',
-        inputs=[Input(name='merged_data', type=FlyteFile[TypeVar('csv')], value=task3['merged_data'])],
+        command='ls -lart /workflow && ls -lart /workflow/data && /mnt/code/scripts/setup.sh && ls -lart /workflow && ls -lart /workflow/data && python /mnt/code/scripts/process-data.py',
+        inputs=[Input(name='merged_data', type=FlyteFile[TypeVar('thisisareallylongfiletype')], value=task3['merged_data'])],
         output_specs=[Output(name='processed_data', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
         environment_revision_id=environment_revision_id,
         hardware_tier_name=hardware_tier_name,
         dataset_snapshots=[],
+        netapp_volume_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
@@ -114,7 +122,7 @@ def model_training(data_path_a: str, data_path_b: str):
 
     task5 = run_domino_job_task(
         flyte_task_name='Train Model',
-        command='python /mnt/code/scripts/train-model.py',
+        command='ls -lart /workflow && ls -lart /workflow/data && /mnt/code/scripts/setup.sh && ls -lart /workflow && ls -lart /workflow/data && python /mnt/code/scripts/train-model.py',
         inputs=[
             Input(name='processed_data', type=FlyteFile[TypeVar('csv')], value=task4['processed_data']),
             Input(name='num_estimators', type=int, value=100)],
@@ -123,6 +131,7 @@ def model_training(data_path_a: str, data_path_b: str):
         environment_revision_id=environment_revision_id,
         hardware_tier_name=hardware_tier_name,
         dataset_snapshots=[],
+        netapp_volume_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
@@ -131,16 +140,16 @@ def model_training(data_path_a: str, data_path_b: str):
         cache_version="1.0"
     )
 
-    run_launch_export_artifacts_task(
-        spec_list=[
-            ExportArtifactToDatasetsSpec(
-                artifact=DataArtifact,
-                dataset_id="68d1caab4308f65467650ef3",  # Change to the ID of the Dataset in your project you want to export the DATA Artifact to
-            ),
-        ],
-        environment_name=environment_name,
-        hardware_tier_name=hardware_tier_name,
-        use_project_defaults_for_omitted=True,
-    )
+    # run_launch_export_artifacts_task(
+    #     spec_list=[
+    #         ExportArtifactToDatasetsSpec(
+    #             artifact=DataArtifact,
+    #             dataset_id="6983829c26a4823186cd8b40",  # Change to the ID of the Dataset in your project you want to export the DATA Artifact to
+    #         ),
+    #     ],
+    #     environment_name=environment_name,
+    #     hardware_tier_name=hardware_tier_name,
+    #     use_project_defaults_for_omitted=True,
+    # )
 
     return 
